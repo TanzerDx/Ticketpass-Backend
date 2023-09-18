03043a989f938328daff8910df5ed62c7f2a8c0b
Hristo <i499630@fhict.nl>
2023-09-18 21:38:07 +0200

    Completed all controls

--- src/main/java/controller/OrderController.java
+++ src/main/java/controller/OrderController.java
@@ -1,4 +1,39 @@
 package controller;
 
+import business.Interfaces.OrdersUseCases;
+import domain.Objects.Order;
+import domain.OrdersRelated.CreateOrderRequest;
+import domain.OrdersRelated.CreateOrderResponse;
+import domain.OrdersRelated.GetAllOrdersResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/orders")
+@AllArgsConstructor
 public class OrderController {
+
+        private final OrdersUseCases ordersUseCases;
+
+        @PostMapping()
+        public ResponseEntity<CreateOrderResponse> addOrder(@RequestBody CreateOrderRequest request) {
+        CreateOrderResponse response = ordersUseCases.createOrder(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+        }
+
+        @GetMapping
+        public ResponseEntity<GetAllOrdersResponse> getAllOrders(@RequestParam(name = "userId") Long userId) {
+            return ResponseEntity.ok(ordersUseCases.getAllOrders(userId));
+        }
+
+
+
+        @GetMapping("{id}")
+        public ResponseEntity<Order> getOrder(@RequestParam(name = "id", required = false) final long id) {
+        final Order order = ordersUseCases.getOrder(id);
+                return ResponseEntity.ok().body(order);
+                }
+
 }
--- src/main/java/controller/TicketController.java
+++ src/main/java/controller/TicketController.java
@@ -1,4 +1,51 @@
 package controller;
 
+import business.Converters.TicketConverter;
+import business.Interfaces.ConcertsUseCases;
+import business.Interfaces.TicketsUseCases;
+import domain.ConcertsRelated.AddConcertRequest;
+import domain.ConcertsRelated.AddConcertResponse;
+import domain.ConcertsRelated.GetConcertsResponse;
+import domain.ConcertsRelated.UpdateConcertRequest;
+import domain.Objects.Concert;
+import domain.Objects.Ticket;
+import domain.TicketsRelated.AddTicketsRequest;
+import domain.TicketsRelated.AddTicketsResponse;
+import domain.TicketsRelated.UpdateTicketsRequest;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import persistence.entities.TicketEntity;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/tickets")
+@AllArgsConstructor
 public class TicketController {
+
+    private final TicketsUseCases ticketsUseCases;
+
+    @PostMapping()
+    public ResponseEntity<AddTicketsResponse> addTickets(@RequestBody AddTicketsRequest request) {
+        AddTicketsResponse response = ticketsUseCases.addTickets(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<List<Ticket>> getTickets(@PathVariable("id") Long orderId) {
+        List<Ticket> tickets = ticketsUseCases.getTickets(orderId);
+
+        return ResponseEntity.ok().body(tickets);
+    }
+
+    @PutMapping()
+    public ResponseEntity<Void> updateTickets(@RequestBody @Valid UpdateTicketsRequest request) {
+        ticketsUseCases.updateTickets(request);
+
+        return ResponseEntity.noContent().build();
+    }
 }
