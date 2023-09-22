a2b02a977b3513828e22618a3502775334758eec
Hristo <i499630@fhict.nl>
2023-09-22 17:47:38 +0200

    Added some unit tests

--- src/main/java/com/example/individual_assignment_hristo_ganchev/persistence/entities/OrderEntity.java
+++ src/main/java/com/example/individual_assignment_hristo_ganchev/persistence/entities/OrderEntity.java
@@ -1,12 +1,16 @@
 package com.example.individual_assignment_hristo_ganchev.persistence.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderEntity {
 
     private Long id;
--- src/main/java/com/example/individual_assignment_hristo_ganchev/persistence/entities/UserEntity.java
+++ src/main/java/com/example/individual_assignment_hristo_ganchev/persistence/entities/UserEntity.java
@@ -1,12 +1,16 @@
 package com.example.individual_assignment_hristo_ganchev.persistence.entities;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserEntity {
 
     private Long id;
