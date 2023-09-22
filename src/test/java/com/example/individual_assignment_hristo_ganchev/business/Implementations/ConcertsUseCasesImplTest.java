bb7035a131b54de3bcfb31b1c2a2e74612293120
Hristo <i499630@fhict.nl>
2023-09-22 13:23:14 +0200

    Updated Product Backlog and added Git Repository in the documentation

--- src/test/java/com/example/individual_assignment_hristo_ganchev/business/Implementations/ConcertsUseCasesImplTest.java
+++ src/test/java/com/example/individual_assignment_hristo_ganchev/business/Implementations/ConcertsUseCasesImplTest.java
@@ -1,14 +1,12 @@
 package com.example.individual_assignment_hristo_ganchev.business.Implementations;
 
-import com.example.individual_assignment_hristo_ganchev.business.Converters.ConcertConverter;
-import com.example.individual_assignment_hristo_ganchev.business.Interfaces.ConcertsUseCases;
 import com.example.individual_assignment_hristo_ganchev.domain.ConcertsRelated.AddConcertRequest;
 import com.example.individual_assignment_hristo_ganchev.domain.ConcertsRelated.GetConcertsResponse;
-import com.example.individual_assignment_hristo_ganchev.domain.Objects.Concert;
 import com.example.individual_assignment_hristo_ganchev.persistence.entities.ConcertEntity;
 import com.example.individual_assignment_hristo_ganchev.persistence.interfaces.ConcertRepository;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatcher;
+import org.mockito.Mockito;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -16,8 +14,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class ConcertsUseCasesImplTest {
 
@@ -27,22 +24,44 @@ public class ConcertsUseCasesImplTest {
         // Arrange
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
 
-        ConcertRepository concertRepository = mock(ConcertRepository.class);
+        ConcertRepository concertRepositoryMock = mock(ConcertRepository.class);
 
-        AddConcertRequest toAdd = new AddConcertRequest("Chase Atlantic",
-                "Indie", "TivoliVredenburg", "2023/09/04", "Utrecht",
+        ConcertEntity toTest = new ConcertEntity(1L, "Chase Atlantic",
+                "Indie", "TivoliVredenburg", sdf.parse("2023/09/04"), "Utrecht",
                 "Chase Atlantic are an Australian Indie band that became popular in 2015", "URL", 37.15, 1000L);
 
-        when(concertRepository.getAll()).thenReturn(new ArrayList<ConcertEntity>());
+        AddConcertRequest toAdd = new AddConcertRequest("Chase Atlantic",
+                        "Indie", "TivoliVredenburg", "2023/09/04", "Utrecht",
+                        "Chase Atlantic are an Australian Indie band that became popular in 2015", "URL", 37.15, 1000L);
 
-        ConcertsUseCasesImpl sut = new ConcertsUseCasesImpl(concertRepository);
+        when(concertRepositoryMock.addConcert(toTest)).thenReturn(toTest);
+
+        ConcertsUseCasesImpl sut = new ConcertsUseCasesImpl(concertRepositoryMock);
 
         // Act
         sut.addConcert(toAdd);
-        Concert retrievedConcert = sut.getConcert(1L);
+        //Concert retrievedConcert = sut.getConcert(1L);
 
 
         // Assert
+        //assertThat(ConcertConverter.convert(toTest)).isEqualTo(retrievedConcert);
+        Mockito.verify(concertRepositoryMock.addConcert(Mockito.argThat(
+                new ArgumentMatcher<ConcertEntity>() {
+                    @Override
+                    public boolean matches(ConcertEntity argument) {
+                        return toTest.getId().equals(argument.getId()) &&
+                                toTest.getArtist().equals(argument.getArtist()) &&
+                                toTest.getMusicGenre().equals(argument.getMusicGenre()) &&
+                                toTest.getVenue().equals(argument.getVenue()) &&
+                                toTest.getDate().equals(argument.getDate()) &&
+                                toTest.getCity().equals(argument.getCity()) &&
+                                toTest.getDesc().equals(argument.getDesc()) &&
+                                toTest.getPhotoURL().equals(argument.getPhotoURL()) &&
+                                toTest.getPrice().equals(argument.getPrice()) &&
+                                toTest.getTicketsRemaining().equals(argument.getTicketsRemaining());
+                    }
+                }
+        )));
 
     }
 
