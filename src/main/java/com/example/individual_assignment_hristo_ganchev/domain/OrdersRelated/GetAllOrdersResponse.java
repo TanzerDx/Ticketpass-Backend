068e7347a4738f3a91b7c074242ed2aed7ac97d0
Hristo <i499630@fhict.nl>
2023-09-24 16:40:53 +0200

    Created Order and Ticket Unit Tests

--- src/main/java/com/example/individual_assignment_hristo_ganchev/domain/OrdersRelated/GetAllOrdersResponse.java
+++ src/main/java/com/example/individual_assignment_hristo_ganchev/domain/OrdersRelated/GetAllOrdersResponse.java
@@ -1,8 +1,10 @@
 package com.example.individual_assignment_hristo_ganchev.domain.OrdersRelated;
 
 import com.example.individual_assignment_hristo_ganchev.domain.Objects.Order;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
