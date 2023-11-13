4df27236036ca0567dbef8b06ace629b2d014758
Hristo <i499630@fhict.nl>
2023-11-13 19:36:30 +0100

    Implemented partial integration tests for ConcertController

--- src/test/java/com/example/individual_assignment_hristo_ganchev/controller/implementations/ConcertControllerTests.java
+++ src/test/java/com/example/individual_assignment_hristo_ganchev/controller/implementations/ConcertControllerTests.java
@@ -16,6 +16,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -39,9 +40,17 @@ public class ConcertControllerTests {
 
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
 
-        List<Concert> response = Arrays.asList(new Concert(1L, "Kim Petras",
+        List<Concert> response = Arrays.asList(
+
+                new Concert(1L, "Kim Petras",
                 "Pop", "AFAS Live", sdf.parse("2024-02-27T23:00:00.000+00:00"), "Amsterdam",
-                "Kim Petras is a German Pop Star that became popular in 2022.", "URL", 40.05, 1000));
+                "Kim Petras is a German Pop Star that became popular in 2022.", "URL", 40.05, 5000),
+
+                new Concert(2L, "Madison Beer",
+                        "Pop", "AFAS Live", sdf.parse("2024-02-28T23:00:00.000+00:00"), "Amsterdam",
+                        "Madison Beer is an American Pop Star that became popular in 2022.", "URL", 45.05, 5000)
+        );
+
 
         when(concertService.getAllConcerts()).thenReturn(response);
 
@@ -50,28 +59,94 @@ public class ConcertControllerTests {
                 .andExpect(status().isOk())
                 .andExpect(header().string("Content-Type",
                         APPLICATION_JSON_VALUE))
-                .andExpect(jsonPath("$").value("""            
-                    {
-                       "concerts": [
-                         {
-                           "id": "1",
-                           "artist": "Kim Petras",
-                           "musicGenre": "Pop",
-                           "venue": "AFAS Live",
-                           "date": "2024-02-27T23:00:00.000+00:00",
-                           "city": "Amsterdam",
-                           "description": "Kim Petras is a German Pop Star that became popular in 2022.",
-                           "photoURL": "URL",
-                           "price": 40.05,
-                           "ticketsRemaining": 1000
-                         }
-                       ]
-                     }
-                        """));
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$", hasSize(2)))
+
+
+                //Kim Petras
+                .andExpect(jsonPath("$[0].id").value(1))
+                .andExpect(jsonPath("$[0].artist").value("Kim Petras"))
+                .andExpect(jsonPath("$[0].musicGenre").value("Pop"))
+                .andExpect(jsonPath("$[0].venue").value("AFAS Live"))
+                .andExpect(jsonPath("$[0].date").value("2024-02-27T23:00:00.000+00:00"))
+                .andExpect(jsonPath("$[0].city").value("Amsterdam"))
+                .andExpect(jsonPath("$[0].description").value("Kim Petras is a German Pop Star that became popular in 2022."))
+                .andExpect(jsonPath("$[0].photoURL").value("URL"))
+                .andExpect(jsonPath("$[0].price").value(40.05))
+                .andExpect(jsonPath("$[0].ticketsRemaining").value(5000))
+
+
+                //Madison Beer
+                .andExpect(jsonPath("$[1].id").value(2))
+                .andExpect(jsonPath("$[1].artist").value("Madison Beer"))
+                .andExpect(jsonPath("$[1].musicGenre").value("Pop"))
+                .andExpect(jsonPath("$[1].venue").value("AFAS Live"))
+                .andExpect(jsonPath("$[1].date").value("2024-02-28T23:00:00.000+00:00"))
+                .andExpect(jsonPath("$[1].city").value("Amsterdam"))
+                .andExpect(jsonPath("$[1].description").value("Madison Beer is an American Pop Star that became popular in 2022."))
+                .andExpect(jsonPath("$[1].photoURL").value("URL"))
+                .andExpect(jsonPath("$[1].price").value(45.05))
+                .andExpect(jsonPath("$[1].ticketsRemaining").value(5000));
 
         verify(concertService).getAllConcerts();
 
     }
+
+    @Test
+    void getConcerts_shouldReturn200ResponseWithAConcertOfID1() throws Exception {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+
+        Concert response = new Concert(1L, "Kim Petras",
+                        "Pop", "AFAS Live", sdf.parse("2024-02-27T23:00:00.000+00:00"), "Amsterdam",
+                        "Kim Petras is a German Pop Star that became popular in 2022.", "URL", 40.05, 5000);
+
+
+        when(concertService.getConcert(1L)).thenReturn(response);
+
+        mockMvc.perform(get("/concerts/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(header().string("Content-Type",
+                        APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$.id").value(1))
+                .andExpect(jsonPath("$.artist").value("Kim Petras"))
+                .andExpect(jsonPath("$.musicGenre").value("Pop"))
+                .andExpect(jsonPath("$.venue").value("AFAS Live"))
+                .andExpect(jsonPath("$.date").value("2024-02-27T23:00:00.000+00:00"))
+                .andExpect(jsonPath("$.city").value("Amsterdam"))
+                .andExpect(jsonPath("$.description").value("Kim Petras is a German Pop Star that became popular in 2022."))
+                .andExpect(jsonPath("$.photoURL").value("URL"))
+                .andExpect(jsonPath("$.price").value(40.05))
+                .andExpect(jsonPath("$.ticketsRemaining").value(5000));
+
+        verify(concertService).getConcert(1L);
+
+    }
+
+    @Test
+    void getConcerts_shouldReturn200ResponseWithEmptyArray() throws Exception {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+
+        List<Concert> response = new ArrayList<>();
+
+
+        when(concertService.getAllConcerts()).thenReturn(response);
+
+        mockMvc.perform(get("/concerts"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(header().string("Content-Type",
+                        APPLICATION_JSON_VALUE))
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$", hasSize(0)));
+
+        verify(concertService).getAllConcerts();
+
+    }
+
+
 }
 
 
