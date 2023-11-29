763d9ef96eee30c240231621feb70a0aeee2eaa3
Hristo <i499630@fhict.nl>
2023-11-29 02:02:35 +0100

    should fix order unauthorized

--- src/test/java/com/example/individual_assignment_hristo_ganchev/controller/implementations/UserControllerTests.java
+++ src/test/java/com/example/individual_assignment_hristo_ganchev/controller/implementations/UserControllerTests.java
@@ -2,13 +2,17 @@ package com.example.individual_assignment_hristo_ganchev.controller.implementati
 
 
 import com.example.individual_assignment_hristo_ganchev.business.Interfaces.UsersService;
+import com.example.individual_assignment_hristo_ganchev.business.UsersRelated.LoginRequest;
 import com.example.individual_assignment_hristo_ganchev.domain.User;
+import com.example.individual_assignment_hristo_ganchev.persistence.entities.UserEntity;
+import com.example.individual_assignment_hristo_ganchev.persistence.jpa.UserRepository;
 import com.example.individual_assignment_hristo_ganchev.security.token.AccessToken;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -16,6 +20,7 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -31,7 +36,14 @@ public class UserControllerTests {
     private UsersService usersService;
 
     @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
     private AccessToken accessToken;
+
+    @MockBean
+    private PasswordEncoder passwordEncoder;
+
 
     @Test
     @WithMockUser(username = "testuser", roles = {"user"})
@@ -59,6 +71,32 @@ public class UserControllerTests {
         verify(usersService).getUserById(1L);
     }
 
+
+    @Test
+    void Login_shouldReturn200ResponseWithLoggedInUser() throws Exception  {
+
+
+        UserEntity userEntity = new UserEntity(1L, "hristo@gmail.com",
+                "hashedPassword", "user");
+
+        LoginRequest request = new LoginRequest("hristo@gmail.com",
+                "hashedPassword");
+
+
+        when(userRepository.login("hristo@gmail.com")).thenReturn(userEntity);
+        when(passwordEncoder.matches(request.getPassword() , userEntity.getEncodedPassword())).thenReturn(true);
+
+        mockMvc.perform(post("/users/login"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(header().string("Content-Type",
+                        APPLICATION_JSON_VALUE))
+
+                .andExpect(jsonPath("$.accessToken").value("accessToken"));
+
+
+        verify(usersService).Login(request);
+    }
 
 
 }
