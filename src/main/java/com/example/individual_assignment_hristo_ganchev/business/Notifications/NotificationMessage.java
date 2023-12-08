5aabfee4a3eea6c7d3cea19dbf7c0e65a0d178ef
Hristo <i499630@fhict.nl>
2023-12-09 00:04:24 +0100

    should fix ws error

---
+++ src/main/java/com/example/individual_assignment_hristo_ganchev/business/Notifications/NotificationMessage.java
@@ -0,0 +1,15 @@
+package com.example.individual_assignment_hristo_ganchev.business.Notifications;
+
+import com.example.individual_assignment_hristo_ganchev.domain.User;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class NotificationMessage {
+    private String id;
+    private String from;
+    private String topic;
+    private String text;
+
+}
