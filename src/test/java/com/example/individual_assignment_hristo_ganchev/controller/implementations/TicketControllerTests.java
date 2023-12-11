c10fcebc6be98bc1ba84c0e031a90818ce137bb9
Hristo <i499630@fhict.nl>
2023-12-11 20:10:09 +0100

    made some more integration tests

--- src/test/java/com/example/individual_assignment_hristo_ganchev/controller/implementations/TicketControllerTests.java
+++ src/test/java/com/example/individual_assignment_hristo_ganchev/controller/implementations/TicketControllerTests.java
@@ -2,11 +2,14 @@ package com.example.individual_assignment_hristo_ganchev.controller.implementati
 
 
 import com.example.individual_assignment_hristo_ganchev.business.Interfaces.TicketsService;
+import com.example.individual_assignment_hristo_ganchev.business.OrdersRelated.CreateOrderRequest;
+import com.example.individual_assignment_hristo_ganchev.business.TicketsRelated.AddTicketsRequest;
 import com.example.individual_assignment_hristo_ganchev.domain.Concert;
 import com.example.individual_assignment_hristo_ganchev.domain.Order;
 import com.example.individual_assignment_hristo_ganchev.domain.Ticket;
 import com.example.individual_assignment_hristo_ganchev.domain.User;
 import com.example.individual_assignment_hristo_ganchev.security.token.AccessToken;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -14,14 +17,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.hamcrest.Matchers.hasSize;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -39,11 +43,45 @@ public class TicketControllerTests {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @MockBean
     private TicketsService ticketsService;
 
     @MockBean
     private AccessToken accessToken;
+
+    @Test
+    @WithMockUser(username = "testuser", roles = {"user"})
+    void addTickets_shouldReturn201ResponseWithMethodBeingCalledCorrectly() throws Exception{
+
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+
+        Concert concert = new Concert(1L, "Chase Atlantic",
+                "Indie", "TivoliVredenburg", sdf.parse("2024-02-27T23:00:00.000+00:00"), "Utrecht",
+                "Chase Atlantic are an Australian Indie band that became popular in 2015", "URL", 37.15, 1000);
+
+        User user = new User(1L, "hristo@gmail.com",
+                "hashedPassword", "user");
+
+
+        Order order = new Order(1L, concert, user, sdf.parse("2024-02-27T23:00:00.000+00:00"), "Hristo", "Ganchev", "Woenselse Markt 18",
+                "+31613532345", 3, 14.15, "Ideal");
+
+        AddTicketsRequest request = new AddTicketsRequest(order);
+
+
+        mockMvc.perform(post("/tickets")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andExpect(status().isCreated());
+
+
+        verify(ticketsService, atLeastOnce()).addTickets(request);
+    }
 
     @Test
     @WithMockUser(username = "testuser", roles = {"user"})
