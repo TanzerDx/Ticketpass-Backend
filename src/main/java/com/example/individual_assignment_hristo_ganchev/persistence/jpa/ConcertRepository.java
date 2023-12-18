1620ba7071a7614959ba3041e92d5f6c5f1c9f1b
Hristo <i499630@fhict.nl>
2023-12-18 22:03:12 +0100

    queries are now parameterized, security report was made for design diagram

--- src/main/java/com/example/individual_assignment_hristo_ganchev/persistence/jpa/ConcertRepository.java
+++ src/main/java/com/example/individual_assignment_hristo_ganchev/persistence/jpa/ConcertRepository.java
@@ -1,9 +1,10 @@
 package com.example.individual_assignment_hristo_ganchev.persistence.jpa;
 
 import com.example.individual_assignment_hristo_ganchev.persistence.entities.ConcertEntity;
-import com.example.individual_assignment_hristo_ganchev.persistence.entities.UserEntity;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +12,5 @@ import java.util.List;
 @Repository
 public interface ConcertRepository extends JpaRepository<ConcertEntity, Long> {
     @Query("SELECT c FROM ConcertEntity c WHERE c.artist LIKE %:keyword% OR c.musicGenre LIKE %:keyword% OR c.city LIKE %:keyword%")
-    List<ConcertEntity> getByKeyword(String keyword);
+    List<ConcertEntity> getByKeyword(@Param("keyword") String keyword);
 }
