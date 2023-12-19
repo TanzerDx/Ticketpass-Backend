03f027b12e961f75b6fc44c2f3d011e4b7e1c25c
Hristo <i499630@fhict.nl>
2023-12-19 21:58:34 +0100

    finally implemented websockets!

--- src/main/java/com/example/individual_assignment_hristo_ganchev/business/Notifications/WebSocketConfig.java
+++ src/main/java/com/example/individual_assignment_hristo_ganchev/business/Notifications/WebSocketConfig.java
@@ -14,7 +14,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/topic");
-        config.setUserDestinationPrefix("/user");
     }
 
     @Override
