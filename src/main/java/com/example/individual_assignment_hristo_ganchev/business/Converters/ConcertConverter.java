1b710f651ebfeb0881bed626a1401d33ab8b2455
Hristo <i499630@fhict.nl>
2024-01-19 15:57:06 +0100

    Final commit

--- src/main/java/com/example/individual_assignment_hristo_ganchev/business/Converters/ConcertConverter.java
+++ src/main/java/com/example/individual_assignment_hristo_ganchev/business/Converters/ConcertConverter.java
@@ -3,16 +3,20 @@ package com.example.individual_assignment_hristo_ganchev.business.Converters;
 import com.example.individual_assignment_hristo_ganchev.domain.Concert;
 import com.example.individual_assignment_hristo_ganchev.persistence.entities.ConcertEntity;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
 
 public final class ConcertConverter {
 
-    private ConcertConverter()
-    {
+    private ConcertConverter() {
         throw new IllegalStateException("Concert converter");
     }
 
     public static Concert convert(ConcertEntity concert) {
-        if(concert == null) return null;
+        if (concert == null) return null;
 
         return Concert.builder()
                 .id(concert.getId())
@@ -29,7 +33,7 @@ public final class ConcertConverter {
     }
 
     public static ConcertEntity convertToEntity(Concert concert) {
-        if(concert == null) return null;
+        if (concert == null) return null;
 
         return ConcertEntity.builder()
                 .id(concert.getId())
